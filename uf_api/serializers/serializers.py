a4a7dc75bbe9885e394ddd76b86a04623b6070f9
joshu_los1995 <joshu_los1995@outlook.com>
2023-05-11 16:57:30 -0300

    fix

--- uf_api/serializers/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class UfValueSerializer(serializers.Serializer):
-    date = serializers.CharField()
-    uf_value = serializers.CharField()
--- uf_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UfValueView
-
-urlpatterns = [
-    path('uf_value/<str:date>/', UfValueView.as_view(), name='uf_value'),
-]
